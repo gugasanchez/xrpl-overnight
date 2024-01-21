a6b259271955618556997ae131467106c90cef25
pjvperes <pcamposperes2@gmail.com>
2024-01-21 14:11:36 -0300

    Linting

--- contracts/Overnight.sol
+++ contracts/Overnight.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
@@ -6,45 +6,32 @@ import "./math.sol";
 
 interface ITPFt {
     function balanceOf(address) external returns (uint);
-
     function name() external returns (string memory);
-
     function symbol() external returns (string memory);
-
     function decimals() external returns (uint);
-
     function totalSupply() external returns (uint);
-    
     function privilegedTransfer(address, address, uint) external returns (bool);
-
     function getTokenPrice() external returns (uint256);
 }
 
 interface IBRLt {
     function balanceOf(address) external returns (uint);
-
     function name() external returns (string memory);
-
     function symbol() external returns (string memory);
-
     function decimals() external returns (uint);
-
     function totalSupply() external returns (uint);
-    
     function privilegedTransfer(address, address, uint) external returns (bool);
 }
 
-contract Overnight is Ownable, DSMath{
-
+contract Overnight is Ownable, DSMath {
     struct LiquidityProvidedInfo {
         address provider;
         uint256 amount;
     }
 
     address public BRLtAddress;
-    mapping(address => bool) public privilegedAccounts; //Servicos gov e bancos;
+    mapping(address => bool) public privilegedAccounts; // Serviços governamentais e bancos;
     mapping(address => uint256) liquidityProvidersScore; 
-
     uint256 interestRateDaily;
 
     struct liquidityRequest {
@@ -67,7 +54,7 @@ contract Overnight is Ownable, DSMath{
 
     liquidityRequest[] public liquidityRequests;
 
-    constructor(address _BRLtAddress){
+    constructor(address _BRLtAddress) {
         BRLtAddress = _BRLtAddress;
         privilegedAccounts[msg.sender] = true;
         privilegedAccounts[address(this)] = true;
@@ -86,35 +73,31 @@ contract Overnight is Ownable, DSMath{
         privilegedAccounts[account] = false;
     }
 
-    //Passar apenas a parte depois das 3 casas decimais
+    //Take only the part after the 3 decimal places
     //43739 = 1,00043739
-    function setInterestRateDaily (uint256 _newInterestRateDaily) public onlyPrivileged returns (bool){
+    function setInterestRateDaily(uint256 _newInterestRateDaily) public onlyPrivileged returns (bool) {
         interestRateDaily = _newInterestRateDaily;
         return true;
     }
 
-function createLiquidityRequest(
-    string memory _institution,
-    uint256 _totalAmount,
-    address _collateralAsset
-) public onlyPrivileged returns(bool) {
-    // Criar uma nova instância de liquidityRequest diretamente no storage
-    liquidityRequest storage newRequest = liquidityRequests.push();
+    function createLiquidityRequest(string memory _institution, uint256 _totalAmount, address _collateralAsset) public onlyPrivileged returns(bool) {
+        // Criar uma nova instância de liquidityRequest diretamente no storage
+        liquidityRequest storage newRequest = liquidityRequests.push();
 
-    // Definir as propriedades da nova solicitação de liquidez
-    newRequest.institution = _institution;
-    newRequest.institutionAddress = msg.sender;
-    newRequest.totalAmount = _totalAmount;
-    newRequest.raisedAmount = 0;
-    newRequest.collateralAsset = _collateralAsset;
-    newRequest.collateralAmount = (_totalAmount * 10 ** 18) / (ITPFt(_collateralAsset).getTokenPrice());
-    newRequest.requestDate = block.timestamp;
-    newRequest.status = Status.Open;
+        // Definir as propriedades da nova solicitação de liquidez
+        newRequest.institution = _institution;
+        newRequest.institutionAddress = msg.sender;
+        newRequest.totalAmount = _totalAmount;
+        newRequest.raisedAmount = 0;
+        newRequest.collateralAsset = _collateralAsset;
+        newRequest.collateralAmount = (_totalAmount * 10 ** 18) / (ITPFt(_collateralAsset).getTokenPrice());
+        newRequest.requestDate = block.timestamp;
+        newRequest.status = Status.Open;
 
-    privilegedTransferTPFt(_collateralAsset, msg.sender, address(this), newRequest.collateralAmount);
+        privilegedTransferTPFt(_collateralAsset, msg.sender, address(this), newRequest.collateralAmount);
 
-    return true;
-}
+        return true;
+    }
 
     function provideLiquidity(uint256 _liquidityRequestIndex, uint256 _provideAmount) public onlyPrivileged returns (bool) {
         require(_liquidityRequestIndex < liquidityRequests.length, "Request index out of bounds");
@@ -134,67 +117,66 @@ function createLiquidityRequest(
         return true;
     }
 
-function payCreditors(uint256 _liquidityRequestIndex) public onlyPrivileged returns (bool) {
-    require(_liquidityRequestIndex < liquidityRequests.length, "Request index out of bounds");
-    liquidityRequest storage request = liquidityRequests[_liquidityRequestIndex];
+    function payCreditors(uint256 _liquidityRequestIndex) public onlyPrivileged returns (bool) {
+        require(_liquidityRequestIndex < liquidityRequests.length, "Request index out of bounds");
+        liquidityRequest storage request = liquidityRequests[_liquidityRequestIndex];
 
-    require(msg.sender == request.institutionAddress, "This request is not yours!");
+        require(msg.sender == request.institutionAddress, "This request is not yours!");
 
-    for (uint i = 0; i < request.liquidityProviderAddresses.length; i++) {
-        address provider = request.liquidityProviderAddresses[i];
-        uint256 amount = request.liquidityProviders[provider];
+        for (uint i = 0; i < request.liquidityProviderAddresses.length; i++) {
+            address provider = request.liquidityProviderAddresses[i];
+            uint256 amount = request.liquidityProviders[provider];
 
-        if (amount > 0) {
-            uint256 realAmount = (amount * getDailyCompoundedTokenPrice())/(10**18);
+            if (amount > 0) {
+                uint256 realAmount = (amount * getDailyCompoundedTokenPrice())/(10**18);
 
-            privilegedTransferReal(msg.sender, provider, realAmount);
-            //Resetar o montante para evitar re-pagamentos
-            request.liquidityProviders[provider] = 0;
+                privilegedTransferReal(msg.sender, provider, realAmount);
+                // Resetar o montante para evitar re-pagamentos
+                request.liquidityProviders[provider] = 0;
+            }
         }
+        
+        privilegedTransferTPFt(request.collateralAsset, address(this), request.institutionAddress, request.collateralAmount);
+        request.status = Status.Closed;
+
+        return true;
     }
-    
-    privilegedTransferTPFt(request.collateralAsset, address(this), request.institutionAddress, request.collateralAmount);
-    request.status = Status.Closed;
 
-    return true;
+    function defaultPayment(uint256 _liquidityRequestIndex) public onlyPrivileged returns (bool) {
+        require(_liquidityRequestIndex < liquidityRequests.length, "Request index out of bounds");
+        liquidityRequest storage request = liquidityRequests[_liquidityRequestIndex];
+        require(request.status == Status.Open, "Status closed");
+
+        // Verificar se já passaram 24 horas desde a requestDate
+        //require(block.timestamp >= (request.requestDate + 24 hours), "Cannot default before 24 hours");
+        require(block.timestamp >= (request.requestDate + 86400), "Cannot default before 24 hours");
+
+        uint256 amountProvided = request.liquidityProviders[msg.sender];
+        require(amountProvided > 0, "No liquidity provided by sender");
+
+        // Calcular a proporção do colateral que deve ser devolvida
+        uint256 collateralToReturn = (amountProvided * 10 ** 18 / ITPFt(request.collateralAsset).getTokenPrice());
+
+        // Transferir o colateral proporcional de volta para o msg.sender
+        privilegedTransferTPFt(request.collateralAsset, address(this), msg.sender, collateralToReturn);
+
+        // Atualizar o montante levantado e o montante do colateral restante na solicitação
+        request.raisedAmount -= amountProvided;
+        request.collateralAmount -= collateralToReturn;
+
+        // Remover a contribuição do provedor de liquidez
+        request.liquidityProviders[msg.sender] = 0;
+
+        return true;
     }
 
-function defaultPayment(uint256 _liquidityRequestIndex) public onlyPrivileged returns (bool) {
-    require(_liquidityRequestIndex < liquidityRequests.length, "Request index out of bounds");
-    liquidityRequest storage request = liquidityRequests[_liquidityRequestIndex];
-    require(request.status == Status.Open, "Status closed");
+    function privilegedTransferReal(address _from, address _to, uint256 _amount) public onlyPrivileged {
+        IBRLt(BRLtAddress).privilegedTransfer(_from, _to, _amount);
+    }
 
-    // Verificar se já passaram 24 horas desde a requestDate
-    //require(block.timestamp >= (request.requestDate + 24 hours), "Cannot default before 24 hours");
-    require(block.timestamp >= (request.requestDate + 86400), "Cannot default before 24 hours");
-
-
-    uint256 amountProvided = request.liquidityProviders[msg.sender];
-    require(amountProvided > 0, "No liquidity provided by sender");
-
-    // Calcular a proporção do colateral que deve ser devolvida
-    uint256 collateralToReturn = (amountProvided * 10 ** 18 / ITPFt(request.collateralAsset).getTokenPrice());
-
-    // Transferir o colateral proporcional de volta para o msg.sender
-    privilegedTransferTPFt(request.collateralAsset, address(this), msg.sender, collateralToReturn);
-
-    // Atualizar o montante levantado e o montante do colateral restante na solicitação
-    request.raisedAmount -= amountProvided;
-    request.collateralAmount -= collateralToReturn;
-
-    // Remover a contribuição do provedor de liquidez
-    request.liquidityProviders[msg.sender] = 0;
-
-    return true;
-}
-
-  function privilegedTransferReal(address _from, address _to, uint256 _amount) public onlyPrivileged {
-    IBRLt(BRLtAddress).privilegedTransfer(_from, _to, _amount);
-  }
-
-  function privilegedTransferTPFt(address _TPFtAddress, address _from, address _to, uint256 _amount) public onlyPrivileged {
-    ITPFt(_TPFtAddress).privilegedTransfer(_from, _to, _amount);
-  }
+    function privilegedTransferTPFt(address _TPFtAddress, address _from, address _to, uint256 _amount) public onlyPrivileged {
+        ITPFt(_TPFtAddress).privilegedTransfer(_from, _to, _amount);
+    }
 
     function getDailyCompoundedTokenPrice() public view returns (uint256) {
         uint256 ONE_DAY_IN_SECONDS = 24 * 60 * 60;
@@ -218,5 +200,5 @@ function defaultPayment(uint256 _liquidityRequestIndex) public onlyPrivileged re
         }
 
         return price;
-}
+    }
 }
